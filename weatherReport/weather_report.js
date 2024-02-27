d1cc502473343bea2911e4152e5497acfa56b07a
Chris Farah <farahca@mail.uc.edu>
2024-02-27 13:16:15 -0500

    message

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,22 @@
+
+function showweatherDetails(event) {
+    event.preventDefault();
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+    const apiKey = 'f9cf0b169a003f655af1af28f6a764ab'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}`;
+}
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+  const weatherInfo = document.getElementById('weatherInfo');
+  weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                          <p>Temperature: ${data.main.temp} &#8451;</p>
+                          <p>Weather: ${data.weather[0].description}</p>`;
+})
+.catch(error => {
+    console.error('Error fetching weather:', error);
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+  });
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
